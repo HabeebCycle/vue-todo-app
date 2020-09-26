c87695d47620ec4ae6a7de7a0c33eb8b984a6b45
Habeeb Okunade <guessy2k2@yahoo.com>
2020-09-26 16:37:30 +1000

    Completed Project

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<script>
+  import Header from "./components/layout/Header";
+
+  export default {
+    name: "App",
+    components: {
+      Header,
+    },
+  };
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /*text-align: center;*/
+    color: #2c3e50;
+  }
+
+  #nav {
+    padding: 0;
+  }
+
+  #nav a {
+    font-weight: bold;
+    color: #ffffff;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
+</style>
